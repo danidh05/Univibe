f02543c4fc8bc62e6dcca2418042b0a90c74c02b
RalpnAntoine <ralphdaher6@gmail.com>
2024-09-10 15:12:57 +0300

    Added Factories
    Added factories for my models

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -23,12 +24,18 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+
+        $role = Role::first() ?? Role::factory()->create();
+
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role_id' => $role->id,
+            'bio' => 'bio',
+            'is_verified' => 1,
         ];
     }
 
