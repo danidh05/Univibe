416e02345a22dc76056acd0fd507fde4e5dbb1b1
AhmadGh18 <=>
2024-09-11 10:52:25 +0300

    fixing api code

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmailVerficationController;
 use App\Models\User;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
@@ -10,36 +11,22 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/email/verify/{id}/{hash}', function (Request $request, $id, $hash) {
-    $user = User::findOrFail($id);
-
-    if (! hash_equals($hash, sha1($user->getEmailForVerification()))) {
-        return response()->json(['message' => 'Invalid verification link.'], 400);
-    }
-
-    if ($user->hasVerifiedEmail()) {
-        return response()->json(['message' => 'Email is already verified.']);
-    }
-    $user->markEmailAsVerified();
-    $user->isVerified = true;
-    $user->save();
-    return response()->json(['message' => 'Email verified successfully.']);
-})->name('verification.verify');
+Route::get('/email/verify/{id}/{hash}', [EmailVerficationController::class, "verfiy"])->name('verification.verify');
 
 
-Route::get('/email/resend', function (Request $request) {
-    /** @var \App\Models\User $user */
-    $user = $request->user();
+// Route::get('/email/resend', function (Request $request) {
+//     /** @var \App\Models\User $user */
+//     $user = $request->user();
 
 
-    if ($user->hasVerifiedEmail()) {
-        return response()->json(['message' => 'Your email is already verified.'], 400);
-    }
+//     if ($user->hasVerifiedEmail()) {
+//         return response()->json(['message' => 'Your email is already verified.'], 400);
+//     }
 
-    $user->sendEmailVerificationNotification();
+//     $user->sendEmailVerificationNotification();
 
-    return response()->json(['message' => 'Verification email has been resent.']);
-})->middleware(['auth:sanctum', 'throttle:6,1']);
+//     return response()->json(['message' => 'Verification email has been resent.']);
+// })->middleware(['auth:sanctum', 'throttle:6,1']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
