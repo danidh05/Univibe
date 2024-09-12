085b3587cd6118ebd5d14cf0f9f55c5b48e6632a
RalpnAntoine <ralphdaher6@gmail.com>
2024-09-12 11:33:20 +0300

    Added middlewares to the routes
    Added middlewares to the routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\GroupMembersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MessageController;
+use App\Http\Middleware\CheckGroupMember;
+use App\Http\Middleware\CheckGroupOwner;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -17,9 +19,10 @@ Route::delete('/messages/delete', [MessageController::class, 'deletePrivateMessa
 
 Route::post('/groups/create', [GroupController::class, 'createGroup']);
 Route::get('/groups/get', [GroupController::class, 'getMyGroups']);
-Route::put('/groups/update/name', [GroupController::class, 'updateGroupName']);
-Route::put('/groups/update/photo', [GroupController::class, 'updateGroupPhoto']); // there's a problem with sending data in "form-data"
-Route::delete('/groups/delete', [GroupController::class, 'deleteGroup']);
+Route::put('/groups/update/name', [GroupController::class, 'updateGroupName'])->middleware(CheckGroupOwner::class);
+Route::put('/groups/update/photo', [GroupController::class, 'updateGroupPhoto'])->middleware(CheckGroupOwner::class); // there's a problem with sending data in "form-data"
+Route::delete('/groups/delete', [GroupController::class, 'deleteGroup'])->middleware(CheckGroupOwner::class);
 
-Route::post('/group/members/add', [GroupMembersController::class, 'add']);
-Route::post('/group/members/remove', [GroupMembersController::class, 'remove']); // either admin removing someone or someone removing himself
+Route::post('/group/members/add', [GroupMembersController::class, 'add'])->middleware(CheckGroupOwner::class);
+Route::post('/group/members/remove', [GroupMembersController::class, 'remove'])->middleware(CheckGroupOwner::class); // either admin removing someone or someone removing himself
+Route::post('/group/members/leave', [GroupMembersController::class, 'leave'])->middleware(CheckGroupMember::class);
