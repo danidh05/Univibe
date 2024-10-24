1956db56660316f8e81b1e9f1d165680e1cd1486
Ralph <ralphdaher6@gmail.com>
2024-10-24 16:28:15 +0300

    Created model and migration
    Created model and migration for group chat messages

---
+++ app/Models/GroupMessage.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupMessage extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'sender_id',
+        'receiver_group_id',
+        'content',
+        'media_url',
+        'message_type',
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(GroupChat::class, 'receiver_group_id');
+    }
+}
---
+++ database/migrations/2024_10_24_104430_create_group_chat_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_chat_messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_group_id')->constrained('group_chats')->onDelete('cascade');
+            $table->text('content');
+            $table->text('media_url')->nullable();
+            $table->enum('message_type', ['text', 'image', 'video']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_chat_messages');
+    }
+};
