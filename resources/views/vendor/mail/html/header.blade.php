641d8dd3d09c55ffacf0d2c1584f65aeb4f3f75c
AhmadGh18 <=>
2024-10-30 18:19:39 +0800

    A

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+<img src="https://raw.githubusercontent.com/AhmadGh18/img/refs/heads/main/logo%201.png?token=GHSAT0AAAAAACZXORU74DLRDUWNFCHRKTWKZZCA7XA" class="logo">
+
 </a>
 </td>
 </tr>
